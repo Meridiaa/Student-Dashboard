64b956001426be1d2b52751ec03e0d3ec72b1c56
monet <prachi0512.ps@gmail.com>
2026-08-12 10:37:48 +0530

    Add task completion functionality

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ addTaskButton.addEventListener("click", function () {
     const task = document.createElement("li");
 
     task.textContent = taskText;
+    
+    task.addEventListener("click", function () {
+    task.classList.toggle("completed");
+});
 
     taskList.appendChild(task);
 
